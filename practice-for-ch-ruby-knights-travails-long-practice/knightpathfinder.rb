2ce8ccda1bccf1e9a475f7dc749cdb3dbe1354a1
Taisia Karaseva <taisiat@gmail.com>
2023-02-17 16:50:09 -0800

    Add initial class logic for logic for initialize and avalid moves

---
+++ practice-for-ch-ruby-knights-travails-long-practice/knightpathfinder.rb
@@ -0,0 +1,11 @@
+class KnightPathFinder
+    def self.valid_moves(pos)
+        moves = []
+        x, y = pos
+        #[4,4]
+    end
+
+    def initialize(starting_pos) #[num, num]
+        @considered_positions = [starting_pos]
+    end
+end
