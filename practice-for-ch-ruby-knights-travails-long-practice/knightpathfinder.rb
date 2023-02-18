7d9f1fccec16bf376b3810b01fa5e9535e2e17c8
Leah Seyoum <leahseyoum@Leahs-MacBook-Air.local>
2023-02-17 17:07:05 -0800

    ::valid_moves

--- practice-for-ch-ruby-knights-travails-long-practice/knightpathfinder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice/knightpathfinder.rb
@@ -2,7 +2,10 @@ class KnightPathFinder
     def self.valid_moves(pos)
         moves = []
         x, y = pos
-        #[4,4]
+        #[4,4] starting
+     #[6,5] , [6,3], [2, 5], [2, 3], [5, 6], [5, 2], 
+     #[3, 6] [3, 2]
+
     end
 
     def initialize(starting_pos) #[num, num]
