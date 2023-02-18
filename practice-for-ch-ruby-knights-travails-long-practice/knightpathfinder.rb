877280f7b1f0c3890b6d73c9ffd16bbd867a9ef5
Taisia Karaseva <taisiat@gmail.com>
2023-02-17 17:29:45 -0800

    Add valid moves logic and start new move positions loogic

--- practice-for-ch-ruby-knights-travails-long-practice/knightpathfinder.rb
+++ practice-for-ch-ruby-knights-travails-long-practice/knightpathfinder.rb
@@ -1,14 +1,22 @@
 class KnightPathFinder
+    DELTAS = [[2, -1], [2, 1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, -2], [-1, 2]]
     def self.valid_moves(pos)
         moves = []
         x, y = pos
-        #[4,4] starting
-     #[6,5] , [6,3], [2, 5], [2, 3], [5, 6], [5, 2], 
-     #[3, 6] [3, 2]
-
+        DELTAS.each do |delta|
+            x_d, y_d = delta
+            if (x + x_d < 8 && y + y_d < 8) && (x + x_d > -1 && y + y_d > -1)
+                moves.push([x + x_d, y + y_d])
+            end
+        end
+        moves
     end
 
     def initialize(starting_pos) #[num, num]
         @considered_positions = [starting_pos]
+    end
+
+    def new_move_positions(pos)
+        
     end
 end
